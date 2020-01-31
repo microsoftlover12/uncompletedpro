eb7a9af2014cb297d3272c8dd14676ee46274842
microsoftlover12 <39599017+microsoftlover12@users.noreply.github.com>
2020-01-31 14:43:21 +0100

    Add files via upload

---
+++ projmain.js
@@ -0,0 +1,13 @@
+function myfunction(){
+
+location.replace("C:\Users\songul\Desktop\tesoki\single.html");
+
+
+}
+
+
+function loginFunction(){
+
+
+    location.replace("C:\Users\songul\Desktop\tesoki\proj22.html");
+}
